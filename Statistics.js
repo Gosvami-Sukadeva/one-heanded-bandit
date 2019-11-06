f22b866907eb377313bac6e7273ebdcb0bb7de3e
Mateusz Pilarczyk <bhakta666@gmail.com>
2019-11-06 20:08:27 +0100

    add code to Statistics class

--- Statistics.js
+++ Statistics.js
@@ -1,7 +1,34 @@
 class Statistics {
     constructor() {
-        this.gameResult = [];
+        this.gameResults = [{
+            win: true,
+            bid: 2
+        }, {
+            win: true,
+            bid: 12
+        }, {
+            win: false,
+            bid: -10
+        }];
+
     }
+    addGameToStatistics(win, bid) {
+        let gameResult = {
+            win,
+            bid
+        }
+        console.log(gameResult);
+        this.gameResult.push(gameResult)
+    }
+
+    showGameStatistics() {
+        let games = this.gameResults.length;
+        let wins = this.gameResults.filter(result => result.win).length;
+        let losses = this.gameResults.filter(result => !result.win).length;
+        console.log(games, wins, losses);
+        return [games, wins, losses]
+    }
+
 }
 
 const stats = new Statistics()
