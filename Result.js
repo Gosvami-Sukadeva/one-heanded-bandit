56997b68aa0db50f7d0a41b57ab62c32821c45a1
Mateusz Pilarczyk <bhakta666@gmail.com>
2019-11-07 22:01:40 +0100

    class Result is finished

--- Result.js
+++ Result.js
@@ -3,6 +3,9 @@ class Result {
         if (result) return 3 * bid;
         else return 0;
     }
-}
 
-Result.moneyWinInGame(true, 12)
+    static checkWinner(draw) {
+        if (draw[0] === draw[1] && draw[1] === draw[2] || draw[0] !== draw[1] && draw[1] !== draw[2] && draw[0] !== draw[2]) return true;
+        else return false
+    }
+}
